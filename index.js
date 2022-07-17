33f105114768ec6c38083dc2539d521a9880ebb9
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2022-07-17 15:35:53 +0300

    changed framework from node-telegram-bot-api to telegraf

--- index.js
+++ index.js
@@ -1,3 +1,118 @@
+//* telegraf
+
+const { Telegraf } = require("telegraf")
+const dotenv = require("dotenv")
+const axios = require("axios")
+
+dotenv.config()
+const { BOT_TOKEN, KOZUBSKYI_CHAT_ID, LENA_RAK_CHAT_ID, DATABASE_URL } = process.env
+
+const bot = new Telegraf(BOT_TOKEN)
+
+function start() {
+  const sweet = "lena_rak_05"
+  const creator = "kozubskyi"
+  const sweetChatId = Number(LENA_RAK_CHAT_ID)
+  const creatorChatId = Number(KOZUBSKYI_CHAT_ID)
+
+  bot.on("text", async (ctx) => {
+    const firstName = ctx.message.from.first_name
+    const lastName = ctx.message.from.last_name
+    const username = ctx.message.from.username
+    const command = ctx.message.text
+    const chatId = ctx.message.chat.id
+
+    let response
+
+    try {
+      if (username === sweet) {
+        if (command === "/start") {
+          response =
+            "Ленусик, приветик) 😘 Денис просил передать тебе кучу комплиментиков. Напиши или нажми /compliment и получишь комплиментик)"
+        } else if (command === "/compliment") {
+          const { data } = await axios.get(DATABASE_URL)
+          const randomIndex = Math.floor(Math.random() * data.length)
+
+          response = data[randomIndex].text
+        } else {
+          response = "Я передам Денису то, что ты написала) 😘"
+        }
+      } else if (username === creator) {
+        const [adminCommand, newData] = splitMessage(command)
+
+        if (adminCommand === "add") {
+          await axios.post(DATABASE_URL, { text: newData })
+
+          response = "Комплиментик успешно добавлен"
+        } else if (adminCommand === "del") {
+          await axios.delete(`${DATABASE_URL}/${newData}`)
+
+          response = "Комплиментик успешно удален"
+        } else if (adminCommand === "mlr") {
+          ctx.telegram.sendMessage(sweetChatId, newData)
+
+          response = "Сообщение любимой успешно отправлено"
+        } else if (adminCommand === "msg") {
+          const [receiverChatId, text] = splitMessage(newData)
+
+          ctx.telegram.sendMessage(Number(receiverChatId), text)
+          response = "Сообщение пользователю успешно отправлено"
+        } else if (command === "/all") {
+          const { data } = await axios.get(DATABASE_URL)
+
+          response = JSON.stringify(data)
+        } else if (command === "/help") {
+          response =
+            "'add _' - добавить новый комплиментик с текстом _; 'del _' - удалить комплиментик с индексом _; 'cfd _' - перезаписать массив всех элементов на _; 'mlr _' - отправить сообщение Лене Рак с текстом _; 'msg _ __' - отправить сообщение пользователю с id чата _ и текстом __; '/all' - получить массив entries всех комплиментиков."
+        } else {
+          response = "Некорректная команда"
+        }
+      } else {
+        if (command === "/start" || command === "/compliment") {
+          response = "К сожалению Вы не Елена Рак, а комплиментики я делаю только ей 🤷‍♂️"
+        } else {
+          response = "Я передам Денису то, что Вы написали 😉"
+        }
+      }
+
+      ctx.reply(response)
+
+      username !== creator &&
+        ctx.telegram.sendMessage(
+          creatorChatId,
+          `Пользователь "${firstName} ${lastName} <${username}> (${chatId})" отправил(-а) сообщение "${command}" и получил(-а) ответ "${response}"`
+        )
+    } catch (err) {
+      username !== creator && ctx.reply("Я немножко сломался, скоро починюсь и вернусь 👨‍🔧⚙️😊")
+
+      ctx.telegram.sendMessage(
+        creatorChatId,
+        `❌ Ошибка! Пользователь "${firstName} ${lastName} <${username}> (${chatId})" отправил(-а) сообщение "${command}" и получилась ошибка "${err.message}"`
+      )
+    }
+  })
+}
+
+start()
+
+bot.launch()
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"))
+process.once("SIGTERM", () => bot.stop("SIGTERM"))
+
+function splitMessage(msg) {
+  const msgArr = msg.split(" ")
+  let data = msgArr[0]
+  let text = msgArr.slice(1).join(" ")
+
+  return [data, text]
+}
+
+/*
+
+//* node-telegram-bot-api
+
 require("dotenv").config() // Без этого кода на 1-й строке выдает ошибку:
 // node-telegram-bot-api deprecated Automatic enabling of cancellation of promises is deprecated. In the future, you will have to enable it yourself. See https://github.com/yagop/node-telegram-bot-api/issues/319. at node:internal/modules/cjs/loader:1105:14
 // Решение: https://github.com/yagop/node-telegram-bot-api/issues/540
@@ -125,4 +240,5 @@ function splitMessage(msg) {
   return [data, text]
 }
 
+*/
 
